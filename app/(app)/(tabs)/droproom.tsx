a5b70228150e4e83361c80a6296c12f4a4e5a5e7
Tanagon492547 <155215128+Tanagon492547@users.noreply.github.com>
2025-09-18 22:40:03 +0700

    feat: 🎸 เพิ่ม Drop Room

---
+++ app/(app)/(tabs)/droproom.tsx
@@ -0,0 +1,21 @@
+import RoomForm from "@/components/ui/droprooms/RoomForm";
+import TitleText from "@/components/ui/TitleText";
+import { StyleSheet, View } from "react-native";
+
+const DropRoom =()=>{
+  return(
+    <View style={styles.contarin}>
+        <TitleText textTitle="Drop Room" />
+        <RoomForm />
+    </View>
+  )
+} 
+
+const styles = StyleSheet.create({
+  contarin:{
+    width:'100%',
+    flex:1
+  }
+})
+
+export default DropRoom;
