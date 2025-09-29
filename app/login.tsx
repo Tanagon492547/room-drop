b4374e2f88cbc529615c24577045b0ae9032056f
Tanagon492547 <155215128+Tanagon492547@users.noreply.github.com>
2025-09-29 08:17:59 +0700

    fix: 🐛 ลบไอดี  eas

--- app/login.tsx
+++ app/login.tsx
@@ -7,7 +7,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import Checkbox from "expo-checkbox";
 import { Link, router } from "expo-router";
 import React, { useState } from 'react';
-import { Alert, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const mapAuthError = (code?: string) => {
@@ -57,10 +57,6 @@ const SignIn = () => {
       const friendly = mapAuthError(error?.code);
       setErrorMsg(friendly);
       console.log("Login error:", error);
-       Alert.alert(
-      'Login Failed', // Title
-      `Error Code: ${error.code}\n\nMessage: ${error.message}` // Message
-      )
     } finally {
       setSubmitting(false);
     }
